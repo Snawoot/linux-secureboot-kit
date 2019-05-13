ce85ee5e82d2632b1471b24e7d9d7a01874ae2fb
Vladislav Yarmak <vladislav@vm-0.com>
2019-05-14 01:05:26 +0300

    fix uuidgen.sh

--- uuidgen.sh
+++ uuidgen.sh
@@ -4,9 +4,9 @@ set -e
 
 SCRIPT='import uuid;print (str(uuid.uuid4()));'
 
-UUID=$(uuidgen 2>&1) || \
-UUID=$(python -c "$SCRIPT" 2>&1) || \
-UUID=$(python3 -c "$SCRIPT" 2>&1) || \
-UUID=$(python2 -c "$SCRIPT" 2>&1)
+UUID=$(uuidgen 2>/dev/null) || \
+UUID=$(python -c "$SCRIPT" 2>/dev/null) || \
+UUID=$(python3 -c "$SCRIPT" 2>/dev/null) || \
+UUID=$(python2 -c "$SCRIPT" 2>/dev/null)
 
 echo "$UUID"
