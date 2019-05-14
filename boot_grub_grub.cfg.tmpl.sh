6895809181b43f191e7ede9953da247629139b41
Vladislav Yarmak <vladislav@vm-0.com>
2019-05-14 20:26:39 +0300

    restore original prefix

--- boot_grub_grub.cfg.tmpl.sh
+++ boot_grub_grub.cfg.tmpl.sh
@@ -5,13 +5,17 @@ GRUB2PROBE="$2"
 GRUB2MKRELPATH="$3"
 
 GRUBCFGPATH="$(realpath "$GRUBCFGLINK")"
+GRUBPFXPATH="$(dirname "$GRUBCFGPATH")"
 
 CFGRELPATH="$("$GRUB2MKRELPATH" "$GRUBCFGPATH")"
+PFXRELPATH="$("$GRUB2MKRELPATH" "$GRUBPFXPATH")"
 CFGDEV="$("$GRUB2PROBE" -t device "$GRUBCFGPATH")"
 
 . /usr/share/grub/grub-mkconfig_lib
 
 prepare_grub_to_access_device "$CFGDEV"
+
+echo "set prefix=\"$PFXRELPATH\""
 
 cat <<EOF
 menuentry "Signed Internal Drive" --unrestricted {
