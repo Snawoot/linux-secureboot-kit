2a44debc0e1a59c8b27d065b98d39f47a0b78daf
Vladislav Yarmak <vladislav@vm-0.com>
2019-07-02 19:56:46 +0300

    probe-grub-modules.sh: use temp file for grub image output

--- probe-grub-modules.sh
+++ probe-grub-modules.sh
@@ -5,22 +5,24 @@ set -e
 GRUB2MKIMAGE="$1"
 shift
 
+OUTPUT="$(mktemp)"
+
 # Sanity check
 "$GRUB2MKIMAGE" -p /boot/grub --format=x86_64-efi \
-    --output=log.txt >log.txt || \
+    --output="$OUTPUT" >/dev/null || \
 {
     echo >&2 "E: grub2 image builder is not usable"
+    rm "$OUTPUT"
     exit 1
 }
 
 for i in "$@" ; do
     "$GRUB2MKIMAGE" -p /boot/grub \
         --format=x86_64-efi \
-        --output=log.txt \
-        "$i" >log.txt 2>&1 && \
+        --output="$OUTPUT" \
+        "$i" >/dev/null 2>&1 && \
     echo "$i"
 done
 
-rm log.txt
-
+rm "$OUTPUT"
 exit 0
